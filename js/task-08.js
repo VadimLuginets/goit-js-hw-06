ad42062f0c1654fee0b657b56a0944b49a06b789
Vadim luginets <pebeeo@gmail.com>
2022-08-06 19:14:59 +0300

    Task 8-9
    works

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  if (mail.length === 0) {
+    alert("All fields must be filed");
+  }
+  if (password.length === 0) {
+    alert("All fields must be filed");
+  }
+  console.log(mail);
+  console.log(password);
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector(".change-color");
+const colorToChange = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", () => {
+  colorToChange.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorToChange.textContent;
+});
